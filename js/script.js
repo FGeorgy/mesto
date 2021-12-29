491f99008312840b7beacd078a196209f11dffaa
FGeorgy <f05g29@yandex.ru>
2021-12-30 00:00:53 +0800

    second

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button');
-let inputName = document.querySelector('.popup__input-name');
-let inputAbout = document.querySelector('.popup__input-about');
+let closeButton = popup.querySelector('.popup__close-button');
+let inputName = popup.querySelector('input[name="popup__input-name"]');
+let inputAbout = popup.querySelector('input[name="popup__input-about"]');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
-let saveButton = document.querySelector('.popup__save-button');
+let saveButton = popup.querySelector('.popup__save-button');
 
-inputName.value = profileName.textContent;
-inputAbout.value = profileAbout.textContent;
-
-function close() {
+function closePopup() {
   popup.classList.remove('popup_opened');
 };
 
 function openPopup() {
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
   popup.classList.add('popup_opened');
 }
 
-function formEdit(evt) {
+function editForm(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  close();
+  closePopup();
 }
 
 editButton.addEventListener('click', openPopup);
 
-closeButton.addEventListener('click', close);
+closeButton.addEventListener('click', closePopup);
 
-saveButton.addEventListener('click', formEdit);
+saveButton.addEventListener('click', editForm);
+
+popup.addEventListener('submit', editForm);
