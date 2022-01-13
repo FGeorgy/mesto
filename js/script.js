7a8100acff44f5c4debb08b14ded792aec8ee0c7
FGeorgy <f05g29@yandex.ru>
2022-01-13 19:19:03 +0800

    ПР6 ревью один

--- js/script.js
+++ js/script.js
@@ -26,6 +26,9 @@ const imgCaption = popupZoomImg.querySelector('.popup__caption');
 //Массив кнопок закрытия попапов
 const closeButtons = document.querySelectorAll('.popup__close-button');
 
+//Массив всех форм
+
+
 //Массивы карточек
 const newCards = [];
 const initialCards = [
@@ -106,6 +109,16 @@ function addNewElement () {
 //Функция открытия попапов
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+  popup.addEventListener('click', function (evt) {
+    if (!evt.target.closest('.popup__form')) {
+      closePopup(popup);
+    };
+  });
+  document.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      closePopup(popup);
+    };
+  });
 };
 
 //Функция закрытия попапов
@@ -147,3 +160,72 @@ popupAddElement.addEventListener('submit', function (evt) {
   closePopup(popupAddElement);
   evt.preventDefault();
 });
+
+//Валидация форм
+
+//Функция добавления класса "Ошибка"
+function showError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  errorElement.classList.add('popup__input-error_active');
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+};
+
+//Функция удаления класса "Ошибка"
+function hideError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  errorElement.classList.remove('popup__input-error_active');
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.textContent = '';
+};
+
+//Функция проверки на валидность
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideError(formElement, inputElement);
+  };
+};
+
+function hasInvalidInput(inputs) {
+  return inputs.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+function disableButton(inputs, saveButton) {
+  if (hasInvalidInput(inputs)) {
+    saveButton.classList.add('popup__save-button_inactive');
+    saveButton.setAttribute('disabled', true);
+  } else {
+    saveButton.classList.remove('popup__save-button_inactive');
+    saveButton.removeAttribute('disabled');
+  };
+};
+
+//Функция запуска валидации в инпутах определенного элемента
+function setEventListeners(formElement) {
+  const inputs = Array.from(formElement.querySelectorAll('.popup__input'));
+  const saveButton = formElement.querySelector('.popup__save-button');
+  disableButton(inputs, saveButton);
+  inputs.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement);
+      disableButton(inputs, saveButton);
+    });
+  });
+};
+
+//Функция определения элемента из массива форм
+function enableValidation() {
+  const popupForms = Array.from(document.querySelectorAll('.popup__form'));
+  popupForms.forEach((formElement) => {
+    formElement.addEventListener('submit', () => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+enableValidation();
+
